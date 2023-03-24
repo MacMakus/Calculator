c8fa4c0f74df697d155b8a63e2567019e4512872
MacMakus <119934279+MacMakus@users.noreply.github.com>
2023-03-24 18:21:20 +0700

    Add files via upload

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Numb1_Button = new System.Windows.Forms.Button();
+            this.CalTextBox = new System.Windows.Forms.TextBox();
+            this.Numb2_Button = new System.Windows.Forms.Button();
+            this.Numb3_Button = new System.Windows.Forms.Button();
+            this.Plus_Button = new System.Windows.Forms.Button();
+            this.Result_Button = new System.Windows.Forms.Button();
+            this.Numb4_Button = new System.Windows.Forms.Button();
+            this.Numb5_Button = new System.Windows.Forms.Button();
+            this.Numb6_Button = new System.Windows.Forms.Button();
+            this.Numb7_Button = new System.Windows.Forms.Button();
+            this.Numb8_Button = new System.Windows.Forms.Button();
+            this.Numb9_Button = new System.Windows.Forms.Button();
+            this.Numb0_Button = new System.Windows.Forms.Button();
+            this.Minus_Button = new System.Windows.Forms.Button();
+            this.Multiple_Button = new System.Windows.Forms.Button();
+            this.Divide_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Numb1_Button
+            // 
+            this.Numb1_Button.Location = new System.Drawing.Point(40, 253);
+            this.Numb1_Button.Name = "Numb1_Button";
+            this.Numb1_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb1_Button.TabIndex = 0;
+            this.Numb1_Button.Text = "1";
+            this.Numb1_Button.UseVisualStyleBackColor = true;
+            this.Numb1_Button.Click += new System.EventHandler(this.Numb1_Button_Click);
+            // 
+            // CalTextBox
+            // 
+            this.CalTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.CalTextBox.Location = new System.Drawing.Point(40, 23);
+            this.CalTextBox.Name = "CalTextBox";
+            this.CalTextBox.Size = new System.Drawing.Size(304, 38);
+            this.CalTextBox.TabIndex = 1;
+            this.CalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.CalTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CalBox_KeyPress);
+            // 
+            // Numb2_Button
+            // 
+            this.Numb2_Button.Location = new System.Drawing.Point(121, 253);
+            this.Numb2_Button.Name = "Numb2_Button";
+            this.Numb2_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb2_Button.TabIndex = 2;
+            this.Numb2_Button.Text = "2";
+            this.Numb2_Button.UseVisualStyleBackColor = true;
+            this.Numb2_Button.Click += new System.EventHandler(this.Numb2_Button_Click);
+            // 
+            // Numb3_Button
+            // 
+            this.Numb3_Button.Location = new System.Drawing.Point(202, 253);
+            this.Numb3_Button.Name = "Numb3_Button";
+            this.Numb3_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb3_Button.TabIndex = 3;
+            this.Numb3_Button.Text = "3";
+            this.Numb3_Button.UseVisualStyleBackColor = true;
+            this.Numb3_Button.Click += new System.EventHandler(this.Numb3_Button_Click);
+            // 
+            // Plus_Button
+            // 
+            this.Plus_Button.Location = new System.Drawing.Point(295, 253);
+            this.Plus_Button.Name = "Plus_Button";
+            this.Plus_Button.Size = new System.Drawing.Size(75, 41);
+            this.Plus_Button.TabIndex = 4;
+            this.Plus_Button.Text = "+";
+            this.Plus_Button.UseVisualStyleBackColor = true;
+            this.Plus_Button.Click += new System.EventHandler(this.Plus_Button_Click);
+            // 
+            // Result_Button
+            // 
+            this.Result_Button.Location = new System.Drawing.Point(295, 300);
+            this.Result_Button.Name = "Result_Button";
+            this.Result_Button.Size = new System.Drawing.Size(75, 41);
+            this.Result_Button.TabIndex = 5;
+            this.Result_Button.Text = "=";
+            this.Result_Button.UseVisualStyleBackColor = true;
+            this.Result_Button.Click += new System.EventHandler(this.Result_Button_Click);
+            // 
+            // Numb4_Button
+            // 
+            this.Numb4_Button.Location = new System.Drawing.Point(40, 206);
+            this.Numb4_Button.Name = "Numb4_Button";
+            this.Numb4_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb4_Button.TabIndex = 6;
+            this.Numb4_Button.Text = "4";
+            this.Numb4_Button.UseVisualStyleBackColor = true;
+            this.Numb4_Button.Click += new System.EventHandler(this.Numb4_Button_Click);
+            // 
+            // Numb5_Button
+            // 
+            this.Numb5_Button.Location = new System.Drawing.Point(121, 206);
+            this.Numb5_Button.Name = "Numb5_Button";
+            this.Numb5_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb5_Button.TabIndex = 7;
+            this.Numb5_Button.Text = "5";
+            this.Numb5_Button.UseVisualStyleBackColor = true;
+            this.Numb5_Button.Click += new System.EventHandler(this.Numb5_Button_Click);
+            // 
+            // Numb6_Button
+            // 
+            this.Numb6_Button.Location = new System.Drawing.Point(202, 206);
+            this.Numb6_Button.Name = "Numb6_Button";
+            this.Numb6_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb6_Button.TabIndex = 8;
+            this.Numb6_Button.Text = "6";
+            this.Numb6_Button.UseVisualStyleBackColor = true;
+            this.Numb6_Button.Click += new System.EventHandler(this.Numb6_Button_Click);
+            // 
+            // Numb7_Button
+            // 
+            this.Numb7_Button.Location = new System.Drawing.Point(40, 159);
+            this.Numb7_Button.Name = "Numb7_Button";
+            this.Numb7_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb7_Button.TabIndex = 9;
+            this.Numb7_Button.Text = "7";
+            this.Numb7_Button.UseVisualStyleBackColor = true;
+            this.Numb7_Button.Click += new System.EventHandler(this.Numb7_Button_Click);
+            // 
+            // Numb8_Button
+            // 
+            this.Numb8_Button.Location = new System.Drawing.Point(121, 159);
+            this.Numb8_Button.Name = "Numb8_Button";
+            this.Numb8_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb8_Button.TabIndex = 10;
+            this.Numb8_Button.Text = "8";
+            this.Numb8_Button.UseVisualStyleBackColor = true;
+            this.Numb8_Button.Click += new System.EventHandler(this.Numb8_Button_Click);
+            // 
+            // Numb9_Button
+            // 
+            this.Numb9_Button.Location = new System.Drawing.Point(202, 159);
+            this.Numb9_Button.Name = "Numb9_Button";
+            this.Numb9_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb9_Button.TabIndex = 11;
+            this.Numb9_Button.Text = "9";
+            this.Numb9_Button.UseVisualStyleBackColor = true;
+            this.Numb9_Button.Click += new System.EventHandler(this.Numb9_Button_Click);
+            // 
+            // Numb0_Button
+            // 
+            this.Numb0_Button.Location = new System.Drawing.Point(121, 300);
+            this.Numb0_Button.Name = "Numb0_Button";
+            this.Numb0_Button.Size = new System.Drawing.Size(75, 41);
+            this.Numb0_Button.TabIndex = 12;
+            this.Numb0_Button.Text = "0";
+            this.Numb0_Button.UseVisualStyleBackColor = true;
+            this.Numb0_Button.Click += new System.EventHandler(this.Numb0_Button_Click);
+            // 
+            // Minus_Button
+            // 
+            this.Minus_Button.Location = new System.Drawing.Point(295, 206);
+            this.Minus_Button.Name = "Minus_Button";
+            this.Minus_Button.Size = new System.Drawing.Size(75, 41);
+            this.Minus_Button.TabIndex = 13;
+            this.Minus_Button.Text = "-";
+            this.Minus_Button.UseVisualStyleBackColor = true;
+            this.Minus_Button.Click += new System.EventHandler(this.Minus_Button_Click);
+            // 
+            // Multiple_Button
+            // 
+            this.Multiple_Button.Location = new System.Drawing.Point(295, 159);
+            this.Multiple_Button.Name = "Multiple_Button";
+            this.Multiple_Button.Size = new System.Drawing.Size(75, 41);
+            this.Multiple_Button.TabIndex = 14;
+            this.Multiple_Button.Text = "x";
+            this.Multiple_Button.UseVisualStyleBackColor = true;
+            this.Multiple_Button.Click += new System.EventHandler(this.Multiple_Button_Click);
+            // 
+            // Divide_Button
+            // 
+            this.Divide_Button.Location = new System.Drawing.Point(295, 112);
+            this.Divide_Button.Name = "Divide_Button";
+            this.Divide_Button.Size = new System.Drawing.Size(75, 41);
+            this.Divide_Button.TabIndex = 15;
+            this.Divide_Button.Text = "÷";
+            this.Divide_Button.UseVisualStyleBackColor = true;
+            this.Divide_Button.Click += new System.EventHandler(this.Divide_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.Divide_Button);
+            this.Controls.Add(this.Multiple_Button);
+            this.Controls.Add(this.Minus_Button);
+            this.Controls.Add(this.Numb0_Button);
+            this.Controls.Add(this.Numb9_Button);
+            this.Controls.Add(this.Numb8_Button);
+            this.Controls.Add(this.Numb7_Button);
+            this.Controls.Add(this.Numb6_Button);
+            this.Controls.Add(this.Numb5_Button);
+            this.Controls.Add(this.Numb4_Button);
+            this.Controls.Add(this.Result_Button);
+            this.Controls.Add(this.Plus_Button);
+            this.Controls.Add(this.Numb3_Button);
+            this.Controls.Add(this.Numb2_Button);
+            this.Controls.Add(this.CalTextBox);
+            this.Controls.Add(this.Numb1_Button);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Key_Press);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Numb1_Button;
+        private System.Windows.Forms.TextBox CalTextBox;
+        private System.Windows.Forms.Button Numb2_Button;
+        private System.Windows.Forms.Button Numb3_Button;
+        private System.Windows.Forms.Button Plus_Button;
+        private System.Windows.Forms.Button Result_Button;
+        private System.Windows.Forms.Button Numb4_Button;
+        private System.Windows.Forms.Button Numb5_Button;
+        private System.Windows.Forms.Button Numb6_Button;
+        private System.Windows.Forms.Button Numb7_Button;
+        private System.Windows.Forms.Button Numb8_Button;
+        private System.Windows.Forms.Button Numb9_Button;
+        private System.Windows.Forms.Button Numb0_Button;
+        private System.Windows.Forms.Button Minus_Button;
+        private System.Windows.Forms.Button Multiple_Button;
+        private System.Windows.Forms.Button Divide_Button;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private string save_NumberList1;
+
+        string setOperator;
+
+        private bool set_Plus;
+        private bool set_Minus;
+        private bool set_Multiple;
+        private bool set_Divide;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Numb1_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "1";
+        }
+
+        private void Numb2_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "2";
+        }
+
+        private void Numb3_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "3";
+        }
+
+        private void Numb4_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "4";
+        }
+
+        private void Numb5_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "5";
+        }
+
+        private void Numb6_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "6";
+        }
+
+        private void Numb7_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "7";
+        }
+
+        private void Numb8_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "8";
+        }
+
+        private void Numb9_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "9";
+        }
+
+        private void Numb0_Button_Click(object sender, EventArgs e)
+        {
+            CalTextBox.Text += "0";
+        }
+
+        private void Plus_Button_Click(object sender, EventArgs e)
+        {
+            setOperator = "+";
+            save_NumberList1 = CalTextBox.Text;
+            CalTextBox.Text = "";
+        }
+
+        private void Minus_Button_Click(object sender, EventArgs e)
+        {
+            setOperator = "-";
+            save_NumberList1 = CalTextBox.Text;
+            CalTextBox.Text = "";
+        }
+
+        private void Multiple_Button_Click(object sender, EventArgs e)
+        {
+            setOperator = "*";
+            save_NumberList1 = CalTextBox.Text;
+            CalTextBox.Text = "";
+        }
+
+        private void Divide_Button_Click(object sender, EventArgs e)
+        {
+            setOperator = "/";
+            save_NumberList1 = CalTextBox.Text;
+            CalTextBox.Text = "";
+        }
+
+        private void Result_Button_Click(object sender, EventArgs e)
+        {
+            int convert_List1;
+            int convert_List2;
+            switch (setOperator)
+            {
+                case "+":
+                    convert_List1 = Convert.ToInt32(save_NumberList1);
+                    convert_List2 = Convert.ToInt32(CalTextBox.Text);
+                    CalTextBox.Text = (convert_List1 + convert_List2).ToString();
+                    break;
+                case "-":
+                    convert_List1 = Convert.ToInt32(save_NumberList1);
+                    convert_List2 = Convert.ToInt32(CalTextBox.Text);
+                    CalTextBox.Text = (convert_List1 - convert_List2).ToString();
+                    break;
+                case "*":
+                    convert_List1 = Convert.ToInt32(save_NumberList1);
+                    convert_List2 = Convert.ToInt32(CalTextBox.Text);
+                    CalTextBox.Text = (convert_List1 * convert_List2).ToString();
+                    break;
+                case "/":
+                    convert_List1 = Convert.ToInt32(save_NumberList1);
+                    convert_List2 = Convert.ToInt32(CalTextBox.Text);
+                    CalTextBox.Text = (convert_List1 / convert_List2).ToString();
+                    break;
+            }
+
+
+        }
+
+        private void Key_Press(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar)) 
+            {
+                CalTextBox.Text += e.KeyChar;
+            }
+
+            
+        }
+
+        private void CalBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
